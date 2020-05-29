fda7d04a966c83222c41b1aff8cc00a2e62f6e39
Martin Perosa <prsmtn@gmail.com>
2020-05-29 18:11:10 +0200

    f

--- RobotsCompetitive/Assets/Script/playerScript.cs
+++ RobotsCompetitive/Assets/Script/playerScript.cs
@@ -13,16 +13,16 @@ public class playerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveSpeed = 10;
-        //Define the speed at which the object moves.
+            float moveSpeed = 10;
+            //Define the speed at which the object moves.
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //Get the value of the Horizontal input axis.
+            float horizontalInput = Input.GetAxis("Horizontal");
+            //Get the value of the Horizontal input axis.
 
-        float verticalInput = Input.GetAxis("Vertical");
-        //Get the value of the Vertical input axis.
+            float verticalInput = Input.GetAxis("Vertical");
+            //Get the value of the Vertical input axis.
 
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
-        //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
+            transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
+            //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
     }
 }
